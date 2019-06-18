464accffe17b1c6bc285df1d08769939a095ff2e
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-06-18 00:09:47 -0300

    Refatoração concluida

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,32 +1,52 @@
 var Calculadora = {
+    ADICAO:'+',
+    SUBTRACAO: '-',
+    DIVISAO:'/',
+    MULTIPLICACAO: '*',
+
     adicionar: function(num1, num2) {
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0
-        }
-        
+        return Calculadora.calcular(num1, num2, Calculadora.ADICAO);
+    },
+    subtrair: function(num1, num2) {
+        return Calculadora.calcular(num1, num2, Calculadora.SUBTRACAO);
+    },
+    dividir: function(num1, num2) {
+        if (num2 == 0) {return 'Erro';}
+        return Calculadora.calcular(num1, num2, Calculadora.DIVISAO);
+    },
+    multiplicar: function(num1, num2) {
+        return Calculadora.calcular(num1, num2, Calculadora.MULTIPLICACAO);
+    },
+
+    calcular: (num1, num2, ope) => {
+        var result = 0;
         num1 = parseFloat(num1);
         num2 = parseFloat(num2);
 
-        return num1 + num2;
-    },
-    subtrair: function(num1, num2) {
         if (isNaN(num1) || isNaN(num2)) {
             return 0
         }
-        return num1 - num2;
-    },
-    dividir: function(num1, num2) {
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0
+
+        switch (ope) {
+            case Calculadora.ADICAO:
+             result = num1 + num2;
+             break;
+            case Calculadora.SUBTRACAO:
+             result = num1 - num2;
+             break;
+            case Calculadora.DIVISAO:
+             result = num1 / num2;
+             break;
+            case Calculadora.MULTIPLICACAO:
+             result = num1 * num2;
+             break; 
+            default:
+             result = 0;
+             break;
         }
-        return num1 / num2;
-    },
-    multiplicar: function(num1, num2) {
-        if (isNaN(num1) || isNaN(num2)) {
-            return 0
-        }
-        return num1 * num2;
-    },
+
+        return result;
+    }
 };
 
 //usado na função require do NodeJs
